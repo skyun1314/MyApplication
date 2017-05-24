26f59f79def30ab9cce97f708b4c15be4617be28
赵凯 <skyun520@gmail.com>
2017-05-24 09:13:00 +0800

    跟新了本地歌曲界面

---
+++ app1/src/main/java/com/example/zk/activity/Data.java
@@ -0,0 +1,13 @@
+package com.example.zk.activity;
+
+import android.os.Environment;
+
+/**
+ * Created by zk on 2017/5/23.
+ */
+
+public class Data {
+
+   public static String APP_FILE_PATH= Environment.getExternalStorageDirectory().getAbsolutePath()+"/zkmusic";
+
+}
